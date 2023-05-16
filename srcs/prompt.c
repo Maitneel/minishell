ea837f1f60df9a075317b7d534b133db24052f9e
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-05-17 05:26:33 +0900

    create: quoteで囲われた文字が与えられる時に、quoteが閉じられていない時に続けて入力させる処理

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,30 +6,31 @@
 /*   By: dummy <dummy@example.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 15:27:47 by dummy             #+#    #+#             */
-/*   Updated: 2023/05/17 04:51:57 by dummy            ###   ########.fr       */
+/*   Updated: 2023/05/17 05:25:40 by dummy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_string.h"
 #include "tokenize.h"
+#include <errno.h>
 #include <stdio.h>
 #include <readline/history.h>
 #include <readline/readline.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 
 bool	is_input_end(char *line, char *quote)
 {
 	size_t	i;
 
 	if (line == NULL && *quote == '\0')
-		return true;
+		return (true);
+	if (line == NULL && *quote != '\0')
+		return (false);
 	i = 0;
 	while (line[i] != '\0')
 	{
-		if ((line[i] == '`' || line[i] == '"'))
+		if ((line[i] == '\'' || line[i] == '"'))
 		{
 			if (*quote == line[i])
 				*quote = '\0';
@@ -44,35 +45,37 @@ bool	is_input_end(char *line, char *quote)
 		return (true);
 }
 
+static void	*free_with_return_null(void *ptr)
+{
+	free(ptr);
+	return (NULL);
+}
+
 t_tokenize	*prompt(void)
 {
 	char		*line;
-	// char		quote;
+	char		quote;
 	t_tokenize	*tokenized;
-	// t_string	buffer;
+	t_string	buffer;
 
-	// quote = '\0';
-	// if (init_string(&buffer, DEFAULT_INIT_SIZE))
-	// 	return (NULL);
+	quote = '\0';
+	if (init_string(&buffer, DEFAULT_INIT_SIZE) == NULL)
+		return (NULL);
 	line = readline("minishell$ ");
-	// while (!is_input_end(line, &quote))
-	// {
-	// 	if (push_back_string(&buffer, line) == NULL);
-	// 	{
-	// 		free(line);
-	// 		return NULL;
-	// 	}
-	// 	line = readline("> ");
-	// 	if (line == NULL)
-	// 	{
-	// 		printf("minishell: %s", strerror(errno));
-	// 		return NULL;
-	// 	}
-		
-	// }
-	
-	printf("%s\n", line);
-	add_history(line);
+	while (!is_input_end(line, &quote))
+	{
+		if (push_back_string(&buffer, line) == NULL)
+			free_with_return_null(line);
+		if (push_back_string(&buffer, "\n") == NULL)
+			free_with_return_null(line);
+		free(line);
+		line = readline("> ");
+	}
+	if (push_back_string(&buffer, line) == NULL)
+		free_with_return_null(line);
+	printf("%s\n", buffer.data);
+	add_history(buffer.data);
 	tokenized = tokenize(line);
+	free(buffer.data);
 	return (tokenized);
 }
