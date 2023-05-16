4974742a6d532255b2e39593e9c46df5bdd3546c
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-05-17 05:04:09 +0900

    WIP

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -3,30 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taksaito <taksaito@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: dummy <dummy@example.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 15:27:47 by dummy             #+#    #+#             */
-/*   Updated: 2023/05/14 16:13:08 by taksaito         ###   ########.fr       */
+/*   Updated: 2023/05/17 04:51:57 by dummy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_string.h"
 #include "tokenize.h"
+#include <stdio.h>
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include <errno.h>
 
-t_tokenize *prompt(void)
+bool	is_input_end(char *line, char *quote)
 {
-	char *line;
-	t_tokenize *tokenized;
+	size_t	i;
 
+	if (line == NULL && *quote == '\0')
+		return true;
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if ((line[i] == '`' || line[i] == '"'))
+		{
+			if (*quote == line[i])
+				*quote = '\0';
+			else if (*quote == '\0')
+				*quote = line[i];
+		}
+		i++;
+	}
+	if (*quote == '\'' || *quote == '"')
+		return (false);
+	else
+		return (true);
+}
+
+t_tokenize	*prompt(void)
+{
+	char		*line;
+	// char		quote;
+	t_tokenize	*tokenized;
+	// t_string	buffer;
+
+	// quote = '\0';
+	// if (init_string(&buffer, DEFAULT_INIT_SIZE))
+	// 	return (NULL);
 	line = readline("minishell$ ");
-	printf("%s\n", line);	
+	// while (!is_input_end(line, &quote))
+	// {
+	// 	if (push_back_string(&buffer, line) == NULL);
+	// 	{
+	// 		free(line);
+	// 		return NULL;
+	// 	}
+	// 	line = readline("> ");
+	// 	if (line == NULL)
+	// 	{
+	// 		printf("minishell: %s", strerror(errno));
+	// 		return NULL;
+	// 	}
+		
+	// }
+	
+	printf("%s\n", line);
 	add_history(line);
 	tokenized = tokenize(line);
-
-	return tokenized;
+	return (tokenized);
 }
