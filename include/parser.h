a42f6ebb122c0722980edecea92cf5914c4b0c8b
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-05-25 19:58:13 +0900

    fix: update command structure

--- include/parser.h
+++ include/parser.h
@@ -16,15 +16,34 @@
 # include "tokenize.h"
 # include <stdbool.h>
 
+// typedef struct s_command
+// {
+//     char *name;
+//     int kind; // redirect, builtin, 
+//     char **args;
+//     char *output;
+//     bool next_pipe;
+//     struct s_command *next;
+// }   t_command;
+
 typedef struct s_command
 {
-    char *name;
-    int kind; // redirect, builtin, 
+    // int kind; // redirect, builtin, 
+    char *command_name;
     char **args;
-    char *output;
+    char *input_file;
+    char *output_file;
     bool next_pipe;
     struct s_command *next;
 }   t_command;
+/*
+> Makefile cat > hoge.txt < ./include/env.h 
+// [{
+    "input_file": "./include/env.h",
+    "command": "cat"
+    "output_file": "hoge.txt"
+}]
+*/
 
 t_command   *parse(t_token_manager *token_manager);
 
