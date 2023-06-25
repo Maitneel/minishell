9dc0d277cfc326a56a5d8f4ea4b5f9c39fab5cf3
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-06-25 21:01:57 +0900

    fix: readlineが空文字列を返した時にもう一度readlineをするようにした

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: dummy <dummy@example.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 15:27:47 by dummy             #+#    #+#             */
-/*   Updated: 2023/06/25 16:27:55 by dummy            ###   ########.fr       */
+/*   Updated: 2023/06/25 21:01:25 by dummy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,43 @@
 // 	return (NULL);
 // }
 
+char	*read_prompt(void)
+{
+	char	*line;
+
+	line = NULL;
+	while (line == NULL)
+	{
+		g_signal_info.status = READING_PROMPT;
+		line = readline("minishe$ ");
+		g_signal_info.status = UNDEFINED;
+		if (line == NULL)
+			break ;
+		if (line[0] == '\0')
+		{
+			free(line);
+			line = NULL;
+		}
+	}
+	return (line);
+}
+
 t_token_manager	*prompt(t_env_manager *env_manager)
 {
 	t_token_manager	*tokenized;
 	t_string		buffer;
 	t_string		expanded;
 
-	g_signal_info.status = READING_PROMPT;
-	if (set_string(&buffer, readline("minishell$ ")) == NULL)
+	buffer.data = NULL;
+	while (buffer.data == NULL)
 	{
-		g_signal_info.status = UNDEFINED;
-		write(STDOUT_FILENO, "exit\n", 5);
-		return (NULL);
+		if (set_string(&buffer, read_prompt()) == NULL)
+		{
+			g_signal_info.status = UNDEFINED;
+			write(STDOUT_FILENO, "exit\n", 5);
+			return (NULL);
+		}
 	}
-	g_signal_info.status = UNDEFINED;
 	add_history(buffer.data);
 	if (expand_env(&expanded, &buffer, env_manager) == NULL)
 	{
